d92388986a71864b46ec81b70b3bfc7180ca1ca4
Mohd Ahad <mutantking6435@gmail.com>
2024-11-01 14:14:03 +0530

    Cloud io and Constants completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,3 +1,3 @@
-MONGODB_URL_KEY: str="MONGO_DB_URL"
+MONGODB_URL_KEY: str="mongodb+srv://ahad:ahad@cluster0.tb4jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MONGO_DATABASE_NAME: str="myntra=reviews"
 SESSION_PRODUCT_KEY: str="product_name"
